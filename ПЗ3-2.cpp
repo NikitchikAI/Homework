2b8021af28fc55920010f658781c68776a9cbecc
NikitchikAI <96501142+NikitchikAI@users.noreply.github.com>
2024-06-21 19:18:29 +0300

    Update ПЗ3-2.cpp

--- ПЗ3-2.cpp
+++ ПЗ3-2.cpp
@@ -1,37 +1,80 @@
 #include <iostream>
 
-class IStrategy {
+class AbstractClass {
 public:
-    virtual void execute() = 0;
-    virtual ~IStrategy() {}
+    virtual ~AbstractClass() {}
+
+    void templateMethod() {
+        this->baseOperation1();
+        this->requiredOperations1();
+        this->baseOperation2();
+        this->hook1();
+        this->requiredOperation2();
+        this->baseOperation3();
+        this->hook2();
+    }
+
+protected:
+    void baseOperation1() {
+        std::cout << "AbstractClass: baseOperation1()" << std::endl;
+    }
+
+    void baseOperation2() {
+        std::cout << "AbstractClass: baseOperation2()" << std::endl;
+    }
+
+    void baseOperation3() {
+        std::cout << "AbstractClass: baseOperation3()" << std::endl;
+    }
+
+    virtual void requiredOperations1() = 0;
+    virtual void requiredOperation2() = 0;
+
+    virtual void hook1() {}
+    virtual void hook2() {}
 };
-class Strategy1 : public IStrategy
-{
-public:
-    void execute() override {
-        std::cout << "Strategy 1" << std::endl;
+
+class ConcreteClassA : public AbstractClass {
+protected:
+    void requiredOperations1() override {
+        std::cout << "ConcreteClassA: requiredOperations1()" << std::endl;
+    }
+
+    void requiredOperation2() override {
+        std::cout << "ConcreteClassA: requiredOperation2()" << std::endl;
+    }
+
+    void hook1() override {
+        std::cout << "ConcreteClassA: hook1()" << std::endl;
     }
 };
-class Strategy2 : public IStrategy {
-public:
-    void execute() override {
-        std::cout << "Strategy 2" << std::endl;
+
+class ConcreteClassB : public AbstractClass {
+protected:
+    void requiredOperations1() override {
+        std::cout << "ConcreteClassB: requiredOperations1()" << std::endl;
+    }
+
+    void requiredOperation2() override {
+        std::cout << "ConcreteClassB: requiredOperation2()" << std::endl;
+    }
+
+    void hook2() override {
+        std::cout << "ConcreteClassB: hook2()" << std::endl;
     }
 };
-class Context {
-private:
-    IStrategy* strategy;
-public:
-    Context(IStrategy* strategy) : strategy(strategy) {}
-    void setStrategy(IStrategy* newstrategy) { strategy = newstrategy; }
-    void executeStrategy() { strategy->execute(); }
-};
+
 int main() {
-    Strategy1 strategy1;
-    Strategy2 strategy2;
-    Context context(&strategy1);
-    context.executeStrategy();
-    context.setStrategy(&strategy2);
-    context.executeStrategy();
+    AbstractClass* classA = new ConcreteClassA;
+    AbstractClass* classB = new ConcreteClassB;
+
+
+    classA->templateMethod();
+    std::cout << std::endl;
+    classB->templateMethod();
+
+    delete classA;
+    delete classB;
+
     return 0;
 }
