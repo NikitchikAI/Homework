99a931fd454fef97f159b49f2141776e0e1fba1d
NikitchikAI <96501142+NikitchikAI@users.noreply.github.com>
2024-06-23 23:39:48 +0300

    Add files via upload

---
+++ ПЗ6-2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <memory>
+
+class Subject {
+public:
+    virtual void request() = 0;
+    virtual ~Subject() = default;
+};
+
+class Handler : public Subject {
+public:
+    void request() override {
+        std::cout << "Handler: Processing request..." << std::endl;
+    }
+};
+
+class Proxy : public Subject {
+private:
+    std::unique_ptr<Handler> handler;
+public:
+    void request() override {
+        if (!handler) {
+            handler = std::make_unique<Handler>();
+        }
+        std::cout << "Proxy: Actions before processing request" << std::endl;
+        handler->request();
+        std::cout << "Proxy: Additional actions after processing request" << std::endl;
+    }
+};
+
+int main() {
+    Proxy proxy;
+    proxy.request();
+    return 0;
+}
+
